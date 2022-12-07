ac1a57177b9524d1d4b1fe822213b2796b311edc
TheAttila <Damsem39@gmail.com>
2022-12-07 15:39:12 -0300

    first commit

---
+++ FArrow.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/arrow/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/arrow/config/BoardConfigArrow.mk)
+$(call inherit-product, device/arrow/sepolicy/common/sepolicy.mk)
+-include vendor/arrow/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+
+TARGET_BOOT_ANIMATION_RES := 720
+#ARROW_GAPPS := true
+
